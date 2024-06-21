56b8464bbfed5731e5db18bd8ac240b926bb06d0
JadHajjar <jad@brains-lb.com>
2024-06-21 17:36:59 +0300

    Initial commit

---
+++ RoadBuilder/Mod.cs
@@ -0,0 +1,33 @@
+﻿using Colossal.IO.AssetDatabase;
+using Colossal.Logging;
+
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+
+namespace RoadBuilder
+{
+	public class Mod : IMod
+	{
+		public static ILog Log { get; } = LogManager.GetLogger(nameof(RoadBuilder)).SetShowsErrorsInUI(false);
+		public static Setting Settings;
+
+		public void OnLoad(UpdateSystem updateSystem)
+		{
+			Log.Info(nameof(OnLoad));
+
+			Settings = new Setting(this);
+			Settings.RegisterInOptionsUI();
+			GameManager.instance.localizationManager.AddSource("en-US", new LocaleEN(Settings));
+
+			AssetDatabase.global.LoadSettings(nameof(RoadBuilder), Settings, new Setting(this));
+		}
+
+		public void OnDispose()
+		{
+			Log.Info(nameof(OnDispose));
+
+			Settings?.UnregisterInOptionsUI();
+		}
+	}
+}
