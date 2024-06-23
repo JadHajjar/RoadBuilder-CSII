a4e7518fc063fb5c0684ccd42bf0631316264703
Nick Morrison <nickmorrison09@gmail.com>
2024-06-22 20:29:18 -0400

    Changed divider for drag and drop function

--- RoadBuilder/UI/src/mods/Components/DragAndDropDivider/DragAndDropDivider.tsx
+++ RoadBuilder/UI/src/mods/Components/DragAndDropDivider/DragAndDropDivider.tsx
@@ -1,8 +1,22 @@
+import { useContext } from 'react';
 import styles from './DragAndDropDivider.module.scss';
+import { DragContext } from 'mods/Contexts/DragContext';
+import classNames from 'classnames';
 
 export const DragAndDropDivider = () => {
+    let dragContext = useContext(DragContext);    
+    let containerClasses = classNames(
+        styles.container,
+        {
+            [styles.hidden]: dragContext.netSectionItem === undefined
+        }
+    );
+
+    let x = 'fdas';
+    let y = {x: 32};
+
     return (
-        <div className={styles.container}>
+        <div className={containerClasses}>
             <div className={styles.divider}>
             </div>
         </div>        
