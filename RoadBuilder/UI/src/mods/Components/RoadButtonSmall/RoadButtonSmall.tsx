7fe78b353533297ff309fea1b11b1607d0836ae3
Nick Morrison <nickmorrison09@gmail.com>
2024-06-24 16:20:42 -0400

    Updated Properties Popup and Bottom View appearance

--- RoadBuilder/UI/src/mods/Components/RoadButtonSmall/RoadButtonSmall.tsx
+++ RoadBuilder/UI/src/mods/Components/RoadButtonSmall/RoadButtonSmall.tsx
@@ -2,29 +2,41 @@ import { NetSectionItem } from 'domain/NetSectionItem';
 import styles from './RoadButtonSmall.module.scss';
 import { Button, Tooltip } from 'cs2/ui';
 import { EditPropertiesPopup } from '../EditPropertiesPopup/EditPropertiesPopup';
+import { MouseEvent, MouseEventHandler, useState } from 'react';
 
 type _Props = {
     item: NetSectionItem;
     onDelete: (index: number) => void;
+    onClick?: (index: number, evt: MouseEvent<HTMLDivElement>) => void;
     index: number;
 };
 export const RoadButtonSmall = (props: _Props) => {        
+    let [showProperties, setShowProperties] = useState(false);
+
+    let onMouseEnter = () => {
+        setShowProperties(true);
+    }
+
+    let onMouseLeave = () => {
+        setShowProperties(false);
+    }
+
+    let popup = showProperties? (
+        <EditPropertiesPopup 
+            onDelete={props.onDelete} 
+            index={props.index} 
+            item={props.item}                 
+            />
+    ) : (<></>);
+
     return (
-        <>
-            <Tooltip tooltip={props.item.DisplayName}>
-                <div className={styles.button}>
-                    <div className={styles.frame}>                    
-                    </div>
-                    <img src='Media/Placeholder.svg'/>
-                    <Button 
-                        variant='icon' 
-                        onSelect={props.onDelete.bind(null, props.index)} 
-                        className={styles.closeButton} 
-                        src='Media/Glyphs/Trash.svg'/>
+        <div className={styles.container} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>            
+            <div className={styles.button}>
+                <div className={styles.frame}>                    
                 </div>
-            </Tooltip>
-            {/* <EditPropertiesPopup item={props.item} /> */}
-        </>
-             
+                <img src='Media/Placeholder.svg'/>                    
+            </div>            
+            {popup}
+        </div>            
     )
 }
