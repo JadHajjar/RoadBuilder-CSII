93352a7b2d8240cb1ec97219d6c34775993add56
Nick Morrison <nickmorrison09@gmail.com>
2024-06-24 12:07:46 -0400

    removed tooltips

--- RoadBuilder/UI/src/mods/Components/ActionPopup/ActionPopup.tsx
+++ RoadBuilder/UI/src/mods/Components/ActionPopup/ActionPopup.tsx
@@ -16,32 +16,19 @@ export default (props: {popupPosition: Number2}) => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.relContainer} style={positionStyle}>
-        
-        <Tooltip tooltip={translate(`Tooltip.LABEL[${mod.id}.ClosePanel]`, "Close Panel")}>
-          <Button className={styles.closeIcon} variant="icon" onSelect={cancelPickerAction} focusKey={FOCUS_DISABLED}>
-            <img style={{ maskImage: "url(Media/Glyphs/Close.svg)" }}></img>
-          </Button>
-        </Tooltip>        
-
-        {/* Use as Template */}
-        <Tooltip tooltip="">
-          <Button className={classNames(styles.templateButton, styles.button)} variant="flat" onSelect={createFromTemplate} focusKey={FOCUS_AUTO}>
-            Use as Template
-          </Button>        
-        </Tooltip>          
-        {/* Create from Scratch */}
-        <Tooltip tooltip="">      
-          <Button className={classNames(styles.createButton, styles.button)} onSelect={createFromScratch} variant="flat" focusKey={FOCUS_AUTO}>
-            Create New Prefab
-          </Button>
-        </Tooltip>         
-        {/* Cancel Button */}
-        <Tooltip tooltip="">  
-          <Button className={classNames(styles.cancelButton, styles.button)} onSelect={cancelPickerAction} variant="flat" focusKey={FOCUS_DISABLED}>
-            Cancel
-          </Button>
-        </Tooltip>         
+      <div className={styles.relContainer} style={positionStyle}>        
+        {/* Use as Template */}        
+        <Button className={classNames(styles.templateButton, styles.button)} variant="flat" onSelect={createFromTemplate} focusKey={FOCUS_AUTO}>
+          Use as Template
+        </Button>                
+        {/* Create from Scratch */}        
+        <Button className={classNames(styles.createButton, styles.button)} onSelect={createFromScratch} variant="flat" focusKey={FOCUS_AUTO}>
+          Create New Prefab
+        </Button>      
+        {/* Cancel Button */}        
+        <Button className={classNames(styles.cancelButton, styles.button)} onSelect={cancelPickerAction} variant="flat" focusKey={FOCUS_DISABLED}>
+          Cancel
+        </Button>        
       </div>
     </div>
   );
