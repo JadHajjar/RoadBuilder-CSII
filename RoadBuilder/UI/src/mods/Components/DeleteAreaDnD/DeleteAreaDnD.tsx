ad5cfdc574333ae86455a7fea0b42ed986fe09fa
Nick Morrison <nickmorrison09@gmail.com>
2024-07-20 15:58:51 -0400

    created useDropArea hook

--- RoadBuilder/UI/src/mods/Components/DeleteAreaDnD/DeleteAreaDnD.tsx
+++ RoadBuilder/UI/src/mods/Components/DeleteAreaDnD/DeleteAreaDnD.tsx
@@ -4,25 +4,23 @@ import { useLocalization } from 'cs2/l10n';
 import { useContext, useEffect, useState } from 'react';
 import { DragContext } from 'mods/Contexts/DragContext';
 import classNames from 'classnames';
+import { useDropArea } from 'domain/DragAndDrop/DropArea';
 
 export const DeleteAreaDnD = (props: {onRemove: (index: number) => void}) => {
     let dragCtx = useContext(DragContext);
     let localize = useLocalization();
-    let [hovered, setHovered] = useState(false);
     let tooltip = localize.translate("Prompt[DragToDelete]", "Drag Here to Remove")!;
     let isDragging = dragCtx.roadLane !== undefined;
     let classes = classNames(styles.area, {[styles.hidden]: !isDragging})
 
-    useEffect(() => {
-        console.log(`Released! ${hovered}`);
-        if (dragCtx.mouseReleased && hovered) {
-            props.onRemove(dragCtx.oldIndex!);
-        }
-    }, [dragCtx.mouseReleased])
+    let onDrop = () => {
+        props.onRemove(dragCtx.oldIndex!);
+    }
+    let [areaRef, _] = useDropArea<HTMLDivElement>({onDrop});
     return (
         <div className={classes}>
             <Tooltip tooltip={tooltip} disabled={!isDragging}>
-                <div className={styles.target} onMouseEnter={setHovered.bind(null, true)} onMouseLeave={setHovered.bind(null, false)}>
+                <div className={styles.target} ref={areaRef}>
                     <img src='coui://gameui/Media/Glyphs/Trash.svg'></img>
                 </div>            
             </Tooltip>            
