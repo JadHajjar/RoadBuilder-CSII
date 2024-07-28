947e134dd4faf2fd25e3b8a3cb35382692f67dcb
Nick Morrison <nickmorrison09@gmail.com>
2024-07-27 20:01:50 -0400

    Cleaned up unused imports

--- RoadBuilder/UI/src/index.tsx
+++ RoadBuilder/UI/src/index.tsx
@@ -1,9 +1,7 @@
 import { ModRegistrar } from "cs2/modding";
-import { BottomView } from "mods/BottomView/BottomView";
 import { ModView } from "mods/ModView/ModView";
 import ModIconButton from "mods/Components/ModIconButton/ModIconButton";
 import { VanillaComponentResolver } from "vanillacomponentresolver";
-import ActionPopup from "mods/Components/ActionPopup/ActionPopup";
 import { RemoveVanillaRightToolbar } from "mods/Components/RemoveVanillaRightToolbar";
 
 const register: ModRegistrar = (moduleRegistry) => {
